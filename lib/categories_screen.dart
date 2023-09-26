e84da4575f3adb6ce0a4502e30f10c64ed75cddd
Deji <Deerealboy@gmail.com>
2023-09-26 21:32:20 +0100

    first screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,19 +1,33 @@
+import 'package:delicacies/category_item.dart';
 import 'package:flutter/material.dart';
+
+import 'dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3/2,
-          mainAxisExtent: 200,
-          crossAxisSpacing: 200,
-          mainAxisSpacing: 200
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Delicacies"),
       ),
-
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              mainAxisExtent: 200,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: dummyCategories
+              .map(
+                (item) => CategoryItem(title: item.title, color: item.color),
+              )
+              .toList(),
+        ),
+      ),
     );
   }
 }
